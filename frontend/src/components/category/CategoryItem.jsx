32523a84c15fff0128ed70564d629c8b9c0cf127
Harsh <harshstudy29@gmail.com>
2024-10-24 15:27:03 +0530

    category responsiveness

--- frontend/src/components/category/CategoryItem.jsx
+++ frontend/src/components/category/CategoryItem.jsx
@@ -1,16 +1,18 @@
+import React from "react";
+
 const CategoryItem = ({ title, description, image }) => {
     return (
         <div className="w-[80vw] sm:w-[50vw] h-[60vh] flex-shrink-0 flex flex-col sm:flex-row bg-blend-normal rounded-lg">
             <img 
                 src={image} 
                 alt={title} 
-                className="w-full sm:w-1/2 h-full object-cover"
+                className="w-full sm:w-1/2 aspect-square h-full object-cover"
             />
             <div className="flex flex-col justify-center p-6 sm:w-1/2">
                 <h2 className="text-3xl  font-bold text-gray-800">{title}</h2>
                 <p className="mt-4 text-gray-600">{description}</p>
-                <button className="mt-6 py-2 px-4 bg-[#271212] text-white font-semibold rounded hover:bg-[#3e1e1e]">
-                    Get {title}
+                <button className="mt-6 rounded-full max-w-28 py-2 bg-[#271212] text-white font-semibold hover:bg-[#3e1e1e]">
+                    Get
                 </button>
             </div>
         </div>
