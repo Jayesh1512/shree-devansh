c09bf9e2ae888b5d4fdd6356d5c223a6a5bdbc9c
Harsh <harshstudy29@gmail.com>
2024-10-26 14:13:52 +0530

    changes get in category

--- frontend/src/components/category/CategoryItem.jsx
+++ frontend/src/components/category/CategoryItem.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const CategoryItem = ({ title, description, image }) => {
+    const navigate = useNavigate();
+
     return (
         <div className="mx-auto mb-10 w-[75vw] sm:w-[50vw] h-[60vh] flex-shrink-0 flex flex-col sm:flex-row bg-blend-normal rounded-lg">
             <img 
@@ -11,7 +14,7 @@ const CategoryItem = ({ title, description, image }) => {
             <div className="flex flex-col justify-center p-6 sm:w-1/2">
                 <h2 className="text-3xl  font-bold text-gray-800">{title}</h2>
                 <p className="mt-4 text-gray-600">{description}</p>
-                <button className="mt-6 rounded-full max-w-28 py-2 bg-[#271212] text-white font-semibold hover:bg-[#3e1e1e]">
+                <button className="mt-6 rounded-full max-w-28 py-2 bg-[#271212] text-white font-semibold hover:bg-[#3e1e1e]" onClick={() => navigate("/product")}  >
                     Get
                 </button>
             </div>
