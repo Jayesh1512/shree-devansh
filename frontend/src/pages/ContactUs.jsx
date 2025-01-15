8984aa96219fba25915dec56b106e6f91c319e76
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-01-15 19:55:29 +0530

    Update ContactUs.jsx

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -66,7 +66,7 @@ const ContactUs = () => {
 
     if (validateForm()) {
       try {
-        const response = await fetch('http://localhost:5000/api/contact', {
+        const response = await fetch('https://shree-devansh.vercel.app/api/contact', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
